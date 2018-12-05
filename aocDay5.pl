1de9ba218ec2267d4bc76a7a9c2de0f3c2142db0
tja52 <taylor.j.atkinson@gmail.com>
2018-12-05 12:05:20 -0500

    Update aocDay5.pl

--- aocDay5.pl
+++ aocDay5.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 
 my @letters;
 
@@ -8,53 +8,30 @@ while (my $line = <INFILE>){
     @letters = split ("", $line);
 }
 
-print "I have read in an array with terminal index $#letters.\n";
-
 my %uniqueletters;
-
-for my $y (0..$#letters){
-    $uniqueletters{lc($letters[$y])} = 0;
-}
+for my $y (0..$#letters){$uniqueletters{lc($letters[$y])} = 0;}
 
 for my $keyletter (keys %uniqueletters){
+  my @newletters = grep {!/$keyletter/i} @letters;
+
+  READING:
+  my $storedletter = '0';
+  my $storedcase = 'x';
+  my $case = 'y';
+  my $letter = '1';
     
-    my @newletters = grep {!/$keyletter/i} @letters;
-
-READING:
-
-my $storedletter = '0';
-my $storedcase = 'x';
-my $case = 'y';
-my $letter = '1';
-    
- for my $x (0..$#newletters){
-
+    for my $x (0..$#newletters){
         $letter = lc($newletters[$x]);
         if ($newletters[$x] !~ /[a-z]/){$case="u";} else {$case="l";}
-
         if ($storedletter eq $letter && $storedcase ne $case){
-
             my $previousletterindex = $x-1;
             splice (@newletters, $previousletterindex, 2);
-
             $storedletter = $letter;
             $storedcase = $case;
             goto READING;
-            
         }
-        
         $storedletter = $letter;
         $storedcase = $case;
-
+    }
 }
-
-print "After removing all instances of $keyletter and looping my array's terminal index is $#newletters\n";
-
-}
-
 close INFILE;
-
-open (OUTFILE, ">finalletterarray.txt");
-print OUTFILE @letters;
-close OUTFILE;
-
